26d9f13db428cf52ef2d2f2e1390c4ae9dda9140
Ben Lai <ben.lai@ping-works.com.au>
2024-03-27 09:55:23 +1100

    publish check

---
+++ src/platform/Pipelines/PublishItem.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Publishing;
+using Sitecore.Publishing.Pipelines.Publish;
+using Sitecore.Publishing.Pipelines.PublishItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Demo.Edge.Website.Pipelines
+{
+    public class PublishItem :PublishProcessor
+    {
+
+        public override void Process(PublishContext context)
+        {
+            var processedItems = context.ProcessedPublishingCandidates;
+            foreach (var processedItem in processedItems)
+            {
+                Log.Info("[PublishAudit] Publishing Item: " + processedItem.Key.ItemId.ToString(), this);
+            }
+
+
+        }
+    }
+}
